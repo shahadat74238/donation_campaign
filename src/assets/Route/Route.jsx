fa8f259f9d5714e5198cb593ead9256b4174db9e
Shahadat Hosen <shahadat74238@gmail.com>
2023-09-24 21:12:24 +0600

    Creat Assaignment Project is Donation Campaign

---
+++ src/assets/Route/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const route = createBrowserRouter([
+    {
+        path: "/",
+        element: <div>Hello world</div>
+    }
+]);
+
+export default route;
