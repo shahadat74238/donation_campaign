b272a3e39e708a90592b2df679ab06341104a0bf
Shahadat Hosen <shahadat74238@gmail.com>
2023-09-27 00:04:50 +0600

    modify Dontion page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -40,49 +40,49 @@ const Donation = () => {
           <div className="grid grid-cols-1 lg:grid-cols-2 gap-6 max-w-7xl mx-auto my-20">
             {btnShow
               ? category.map((donation) => (
-                  <div
-                    key={donation.id}
-                    style={{ backgroundColor: `${donation.card_bg}` }}
-                    className="grid grid-cols-2 items-center gap-6 rounded-lg"
+                <div
+                key={donation.id}
+                style={{ backgroundColor: `${donation.card_bg}` }}
+                className="grid grid-cols-3 items-center gap-6  rounded-lg"
+              >
+                <div className="col-span-1">
+                  <img
+                    className="h-52 w-full object-cover rounded-l-lg"
+                    src={donation.picture}
+                    alt=""
+                  />
+                </div>
+                <div className="col-span-2">
+                  <span
+                    style={{
+                      backgroundColor: `${donation.category_bg}`,
+                      color: `${donation.text_button_bg}`,
+                    }}
+                    className="py-1 px-3 rounded"
                   >
-                    <div className="col-span-1">
-                      <img
-                        className="h-52 w-full object-cover rounded-l-lg"
-                        src={donation.picture}
-                        alt=""
-                      />
-                    </div>
-                    <div className="col-span-2">
-                      <span
-                        style={{
-                          backgroundColor: `${donation.category_bg}`,
-                          color: `${donation.text_button_bg}`,
-                        }}
-                        className="py-1 px-3 rounded"
-                      >
-                        {donation.category}
-                      </span>
-                      <h1 className="font-semibold text-2xl my-2">
-                        {donation.title}
-                      </h1>
-                      <p
-                        style={{ color: `${donation.text_button_bg}` }}
-                        className="font-semibold text-lg"
-                      >
-                        ${donation.price}
-                      </p>
-                      <NavLink to={`/details/${donation.id}`}>
-                        <button
-                          style={{
-                            backgroundColor: `${donation.text_button_bg}`,
-                          }}
-                          className="py-2 mt-5 px-4 rounded-md text-white text-lg font-semibold"
-                        >
-                          View Details
-                        </button>
-                      </NavLink>
-                    </div>
-                  </div>
+                    {donation.category}
+                  </span>
+                  <h1 className="font-semibold text-2xl my-2">
+                    {donation.title}
+                  </h1>
+                  <p
+                    style={{ color: `${donation.text_button_bg}` }}
+                    className="font-semibold text-lg"
+                  >
+                    ${donation.price}
+                  </p>
+                  <NavLink to={`/details/${donation.id}`}>
+                    <button
+                      style={{
+                        backgroundColor: `${donation.text_button_bg}`,
+                      }}
+                      className="py-2 mt-5 px-4 rounded-md text-white text-lg font-semibold"
+                    >
+                      View Details
+                    </button>
+                  </NavLink>
+                </div>
+              </div>
                 ))
               : category.slice(0, 4).map((donation) => (
                   <div
