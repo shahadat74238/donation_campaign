d4ce48b5b6abcca1e053d55dedda6bd1ea619b88
Shahadat Hosen <shahadat74238@gmail.com>
2023-09-27 17:59:53 +0600

    change Pi chart size

--- src/Pages/Statistics/Statistics.jsx
+++ src/Pages/Statistics/Statistics.jsx
@@ -26,13 +26,13 @@ const Statistics = () => {
 
   return (
     <div className="mx-5 lg:0">
-      <div className="h-screen max-w-7xl mx-aut0">
+      <div className="max-w-7xl mx-auto">
         <div className="flex flex-col justify-center items-center">
           <div>
             <PieChart
               series={[
                 {
-                  outerRadius: 140,
+                  outerRadius: 120,
                   data,
                   arcLabel: getArcLabel,
                 },
@@ -43,7 +43,7 @@ const Statistics = () => {
                   fontSize: 20,
                 },
               }}
-              width={450}
+              width={400}
               height={400}
             />
           </div>
